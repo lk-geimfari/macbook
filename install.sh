ffb33dba0179480f5545fc2236732c2efa3734c5
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-01-13 19:34:52 +0300

    Improve git section

--- install.sh
+++ install.sh
@@ -252,8 +252,31 @@ if ask "Do you want to install Git"; then
     success "⚙️ Configuring Git..."
     git config --global user.name "${USERNAME}"
     git config --global user.email "${USER_EMAIL}"
-  else
-    warning "Skipping configuration of git..."
+
+    if ask "How about GPG signing of commits"; then
+      git config --global commit.gpgsign true
+      read -p "$(success 'Enter your fingerprint: ')" GPG_FINGERPRINT
+      git config --global user.signingkey "${GPG_FINGERPRINT}"
+    fi
+  fi
+
+  if ask "How about aliases for Git"; then
+    git config --global alias.unstage "reset HEAD --"
+    git config --global alias.last "log -1 HEAD"
+    git config --global alias.ci "commit"
+    git config --global alias.cia "commit --amend"
+    git config --global alias.cim "commit -m"
+    git config --global alias.cis "commit -S"
+    git config --global alias.civ "commit -v"
+    git config --global alias.co "checkout"
+    git config --global alias.cp "cherry-pick"
+    git config --global alias.d "diff"
+    git config --global alias.newb "checkout -b"
+    git config --global alias.r "remote"
+    git config --global alias.rv "remote -v"
+    git config --global alias.rb "rebase"
+    git config --global alias.tree "log --graph --oneline --all"
+    git config --global alias.undo "reset --mixed HEAD^"
   fi
 
   if ask "Do you want to install Sourcetree"; then
